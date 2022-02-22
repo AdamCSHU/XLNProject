b758058021e82a0a4b2ecda38d900e7253ec9aae
c0000279SHU <99473716+c0000279SHU@users.noreply.github.com>
2022-02-22 09:23:29 +0000

    The Video Tutorial Series
    Added the tutorial series I followed (linked in discord) up until the database creation.

---
+++ XLN-Home/XLN-Home/Shared/User.cs
@@ -0,0 +1,20 @@
+﻿namespace XLN_Home.Shared
+{
+    public class User
+    {
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public User()
+        {
+
+        }
+        public User(int conID, string fName, string lName)
+        {
+            this.ContactID = conID;
+            this.FirstName = fName;
+            this.LastName = lName;
+        }
+    }
+}
