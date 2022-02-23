34f8295ee2e074ef34183d3a9c20423be6dccb72
c0000279SHU <99473716+c0000279SHU@users.noreply.github.com>
2022-02-23 12:58:39 +0000

    Touched up Nav/Index
    Still working on fixing iframe issue

---
+++ XLN/wwwroot/JavaScript.js
@@ -0,0 +1,19 @@
+﻿
+
+
+/*
+if (window.location !== window.parent.location) {
+    var header = document.getElementById("header");
+    var bottom = document.getElementById("bottom");
+
+    header.style.display = "none";
+    bottom.style.display = "none";
+}
+function inIframe() {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+*/
