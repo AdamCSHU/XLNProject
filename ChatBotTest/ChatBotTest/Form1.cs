366a1e8ce8ca40bfdaa9b917ee5c77f453cd6d7c
EvaYohane <Adamrcampbell134@gmail.com>
2022-02-24 09:06:31 +0000

    Added basic chatbot in C3.
    Currently looking into other languages that may suit our project better when developing a chatbot.

---
+++ ChatBotTest/ChatBotTest/Form1.cs
@@ -0,0 +1,20 @@
+namespace ChatBotTest
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MsgBox.Text = String.Empty;
+        }
+    }
+}
